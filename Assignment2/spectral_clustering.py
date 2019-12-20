f4fc7b6325089266ab02e54adb3098f8b6fd38cb
Marco Palazzo <hello.marcopalazzo@gmail.com>
2019-12-20 14:22:10 +0100

    split data creation

--- Assignment2/spectral_clustering.py
+++ Assignment2/spectral_clustering.py
@@ -1,31 +1,4 @@
-import numpy as np
-from sklearn import cluster, datasets, mixture
-from sklearn.neighbors import kneighbors_graph
-from sklearn.preprocessing import StandardScaler
-from itertools import cycle, islice
-
-# ============
-# Generate datasets. We choose the size big enough to see the scalability
-# of the algorithms, but not too big to avoid too long running times
-# ============
-n_samples = 1500
-noisy_circles = datasets.make_circles(n_samples=n_samples, factor=.5, noise=.05)
-noisy_moons = datasets.make_moons(n_samples=n_samples, noise=.05)
-blobs = datasets.make_blobs(n_samples=n_samples, random_state=8)
-no_structure = np.random.rand(n_samples, 2), None
-
-# Anisotropicly distributed data
-random_state = 170
-X, y = datasets.make_blobs(n_samples=n_samples, random_state=random_state)
-transformation = [[0.6, -0.6], [-0.4, 0.8]]
-X_aniso = np.dot(X, transformation)
-aniso = (X_aniso, y)
-
-# blobs with varied variances
-varied = datasets.make_blobs(n_samples=n_samples, cluster_std=[1.0, 2.5, 0.5], random_state=random_state)
-
-_datasets = [(noisy_circles,2), (noisy_moons,2), (varied, 3), (aniso,3)]
-
+from data_ex3 import make_datasets
 
 import numpy as np
 from sklearn.neighbors import radius_neighbors_graph, kneighbors_graph
@@ -34,6 +7,7 @@ from scipy.cluster.vq import kmeans2
 import matplotlib.pyplot as plt
 import networkx as nx
 
+datasets = make_datasets()
 
 def check_symmetric(a, rtol=1e-05, atol=1e-08):
     return np.allclose(a, a.T, rtol=rtol, atol=atol)
@@ -113,10 +87,10 @@ def spectral_clustering(r, D, sim, laplacian, k):
     W = sim(D, k) # weighted adjacency matrix of D, computed by the sim function
     L = laplacian(W) # symmetric laplacian of W
     w, v = np.linalg.eigh(L) # compute eig. values w and eig. vectors v
-    y = kmeans2(v[:, 1:r], r)[1]
+    y = kmeans2(v[:, 1:r], r)[1] # get the first r eigenvectors
     
     plt.scatter(D[:, 0], D[:, 1], c=y, cmap='viridis')
     return y
 
 i = 0
-spectral_clustering(_datasets[i][1], _datasets[i][0][0], sim_knn, laplacian, k=values[i])
+spectral_clustering(datasets[i][1], datasets[i][0][0], sim_knn, laplacian, k=values[i])
