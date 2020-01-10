15b52081b8f7b6aa6b977875fdd9f311f00eb19e
Abdullah <abdullahsaeed@Abdullahs-MacBook-Pro.local>
2020-01-10 23:16:31 +0100

    meh forward feed

--- Assignment3/HiddenLayer.py
+++ Assignment3/HiddenLayer.py
@@ -1,27 +1,28 @@
+import math
 import numpy as np
 from random import random
 
 
-class NeuralLayer:
+class HiddenLayer:
     """
     Represents a hidden layer in the network. It maintains the neurons 
     in it and the activation values for those neurons. 
-    Incoming values is a property of the neural layer and not
-    a neuron.
+    Incoming values is a property of the neural layer as they are universal
+    to all neurons in the layer.
     """
 
-    def __init__(self, incoming, n = 10, activation="relu"):
+    def __init__(self, incoming, n=10, activation="relu"):
         # initialize n neurons
-        self.neurons = [Neuron(len(incoming), activation) for i in range(n)]
-        self.activation = activation
-        self.incoming = incoming
+        self.neurons = [Neuron(incoming, activation) for i in range(n)]
+        self.input = incoming
         self.activation_values = 0
 
-    def set_activation(self):
-        self.activation_values = [neuron.activation for neuron in self.neurons]
+    def set_activation_values(self):
+        self.activation_values = [neuron.activation_value for neuron in self.neurons]
 
-    def get_activation(self):
+    def get_activation_values(self):
         return self.activation_values
+
 
 class Neuron:
     def __init__(self, incoming, activation, bias=0):
@@ -30,21 +31,29 @@ class Neuron:
         corresponding to the each incoming connection. Weights is randomly initialized.
         """
         self.bias = bias
-        self.weights = [random() for i in range(incoming)]
+        self.input = incoming
+        # random weights for now
+        self.weights = [0.5 for i in range(len(incoming))]
         # value before activation function
         self.value = self.calculate_value(incoming)
         # activation value
-        self.activation = self.activation(activation)
+        self.activation_value = self.calculate_activation(activation)
 
-    def calculate_value(self, incoming) -> None:
+    def calculate_value(self, incoming):
         return sum(np.multiply(self.weights, incoming)) - self.bias
 
-    def calculate_activation(self, activation) -> None:
+    def calculate_activation(self, activation):
         # we decided to use relu as the activation function but this code is just
         # to help scalability
         if activation == "relu":
             return Neuron.relu(self.value)
+        elif activation == "sigmoid":
+            return Neuron.sigmoid(self.value)
 
     @classmethod
-    def relu(cls, value: int) -> float:
+    def relu(cls, value):
         return max(0.0, value)
+
+    @classmethod
+    def sigmoid(cls, x):
+        return float(1 / (1 + math.exp(-x)))
--- Assignment3/main.py
+++ Assignment3/main.py
@@ -1,5 +1,18 @@
 import pandas as pd
+from sklearn.preprocessing import normalize, minmax_scale
+from HiddenLayer import HiddenLayer, Neuron
 
-# import the training data as a dataframe
-df = pd.read_excel("D:/2IIG0/Assignment3/HW3train.xlsx")
+df = pd.read_excel("HW3train.xlsx").drop(['y'], axis=1)
+scaled = (minmax_scale(df.as_matrix()))
+print(scaled[5])
 
+h1 = HiddenLayer(scaled[19])
+h1.set_activation_values()
+print(h1.activation_values)
+
+h2 = HiddenLayer(h1.activation_values)
+h2.set_activation_values()
+print(h2.activation_values)
+
+final = Neuron(h2.activation_values, "sigmoid")
+print(final.activation_value)
